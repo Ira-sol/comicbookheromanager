b731e1b02a8d4b6c580ad5152c45add61ee4f21d
Ira-sol <henryadasojr@gmail.com>
2025-04-20 22:54:30 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@ public class Main {
         HeroManager manager = new HeroManager();
 
         // Add heroes
-        manager.addHero(new Hero("Spider-Man", 85, Affiliation.AVENGERS));
-        manager.addHero(new Hero("Batman", 80, Affiliation.JUSTICE_LEAGUE));
+        manager.addHero(new Hero("Spider-Man", 66, Affiliation.AVENGERS));
+        manager.addHero(new Hero("Batman", 90, Affiliation.JUSTICE_LEAGUE));
         manager.addHero(new Hero("Wolverine", 88, Affiliation.X_MEN));
-        manager.addHero(new Hero("Wonder Woman", 95, Affiliation.JUSTICE_LEAGUE));
+        manager.addHero(new Hero("Super Man", 100, Affiliation.JUSTICE_LEAGUE));
 
         System.out.println("\nOriginal List:");
         manager.displayHeroes();
@@ -24,7 +24,7 @@ public class Main {
         manager.displayHeroes();
 
         System.out.println("\nAdding Storm...");
-        manager.addHero(new Hero("Storm", 87, Affiliation.X_MEN));
+        manager.addHero(new Hero("Storm", 99, Affiliation.X_MEN));
         manager.displayHeroes();
 
         System.out.println("\n2D Array Format:");
